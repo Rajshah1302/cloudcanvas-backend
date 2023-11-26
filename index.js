8845b0d2b3f14ed0f6b1ef91886481add72bc084
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2023-11-26 14:45:39 +0530

    Created Routes and Schema for User and Notes,

--- index.js
+++ index.js
@@ -4,10 +4,13 @@ const app = express()
 const port = 3000
 connectToMongo();
 
-
+//Available Routes 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use('/api/auth',require('./routes/auth'));
+app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
