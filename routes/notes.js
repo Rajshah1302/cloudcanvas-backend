09ca3f52401545c9e57411183b52deeb523c6bb0
Raj Jitendra Shah <141725750+Rajshah1302@users.noreply.github.com>
2023-12-18 19:56:48 +0530

    Implement routes for fetching and adding notes.

--- routes/notes.js
+++ routes/notes.js
@@ -1,9 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const fetchUser = require('../middleware/fetchUser');
+const { body, validationResult } = require('express-validator');
+const Note = require('../models/Note');
 
-router.get('/',(req,res)=>{
+// ROUTE1: Gets all Notes: GET "api/notes/fetchNotes".
+router.get('/fetchNotes', fetchUser, async (req, res) => {
+    try {
+        // Fetch all notes for the authenticated user
+        const notes = await Note.find({ user: req.user.id });
+        
+        // Send the retrieved notes in the response
+        res.json(notes);
+    } catch (error) {
+        // Handle errors, log them, and send an appropriate response
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
-    res.json([]);
-})
+// ROUTE2: Add Notes: POST "api/notes/addNotes".
+router.post('/addNotes', fetchUser, [
+    body('title', 'Invalid title').isLength({ min: 1 }),
+    body('description', 'Description must be at least 5 characters long').isLength({ min: 5 }),
+], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            // If there are validation errors, return a 400 Bad Request with the error details
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        // Destructure values from the request body
+        const { title, description, tag } = req.body;
+
+        // Create a new note with the provided data
+        const note = new Note({
+            title,
+            description,
+            tag,
+            user: req.user.id,
+        });
+
+        // Save the note to the database
+        const savedNote = await note.save();
+
+        // Send the saved note in the response
+        res.json(savedNote);
+    } catch (error) {
+        // Handle errors, log them, and send an appropriate response
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
